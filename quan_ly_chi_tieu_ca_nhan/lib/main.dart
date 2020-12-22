930f2e343a67ccc789e61618fbf67190d1e16961
Trong Hieu <hieuntctk42@gmail.com>
2020-12-22 14:06:33 +0700

    Update

--- quan_ly_chi_tieu_ca_nhan/lib/main.dart
+++ quan_ly_chi_tieu_ca_nhan/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:quan_ly_chi_tieu_ca_nhan/screens/home_page.dart';
 import 'package:quan_ly_chi_tieu_ca_nhan/screens/them_muc_tieu_screen.dart';
 import 'package:quan_ly_chi_tieu_ca_nhan/screens/thunhap_all_screen.dart';
 import 'package:quan_ly_chi_tieu_ca_nhan/screens/thunhap_detail_screen.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(App());
 }
 
-class HomePage extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: IncomeAllScreen(),
+      home: HomeScreen(),
     );
   }
 }
