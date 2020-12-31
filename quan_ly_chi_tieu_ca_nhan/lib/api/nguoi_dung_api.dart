2dabf8d8b91c2a48cd492adb0367c8637013d768
Trong Hieu <hieuntctk42@gmail.com>
2020-12-31 10:46:11 +0700

    Chuc nang tao tai khoan

--- quan_ly_chi_tieu_ca_nhan/lib/api/nguoi_dung_api.dart
+++ quan_ly_chi_tieu_ca_nhan/lib/api/nguoi_dung_api.dart
@@ -4,7 +4,8 @@ import 'package:http/http.dart' as http;
 import 'package:quan_ly_chi_tieu_ca_nhan/models/nguoi_dung.dart';
 
 class NguoiDungApi {
-  final String _urlDangNhap = "http://10.0.2.2:5000/api/dangnhap";
+  final String _urlDangNhap = "http://10.0.3.2:5000/api/dangnhap";
+  final String _urlTaoTaiKhoan = 'http://10.0.3.2:5000/api/taotaikhoan';
 
   Future<NguoiDung> dangNhap(String tenDangNhap, String matKhau) async {
     var body = {"TenDangNhap": tenDangNhap, "MatKhau": matKhau};
@@ -21,6 +22,32 @@ class NguoiDungApi {
     if (response.statusCode == 200) {
       return NguoiDung.fromJson(jsonDecode(response.body));
     }
+    return null;
+  }
+
+  Future<NguoiDung> taoTaiKhoan(String tenDangNhap, String matKhau,
+      String tenHienThi, String avatar) async {
+    var body = {
+      "TenDangNhap": tenDangNhap,
+      "MatKhau": matKhau,
+      "TenHienThi": tenHienThi,
+      "Avatar": avatar,
+    };
+
+    var bodyJson = jsonEncode(body);
+
+    http.Response response = await http.post(
+      _urlTaoTaiKhoan,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: bodyJson,
+    );
+
+    if (response.statusCode == 200) {
+      return NguoiDung.fromJson(jsonDecode(response.body));
+    }
+
     return null;
   }
 }
