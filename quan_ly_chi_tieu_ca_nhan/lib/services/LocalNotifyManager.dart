d5d90b8aee4d7944d34ddbf38fc8ac30316b7a33
Trong Hieu <hieuntctk42@gmail.com>
2021-01-03 21:29:22 +0700

    Thêm local notification 🐱‍👤🐱‍👤🐱‍👤

---
+++ quan_ly_chi_tieu_ca_nhan/lib/services/LocalNotifyManager.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/subjects.dart';
+
+class LocalNotifyManager {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  var initSetting;
+  BehaviorSubject<ReceiveNotification> get didReceiveLocalNotificationSubject =>
+      BehaviorSubject<ReceiveNotification>();
+
+  LocalNotifyManager.init() {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    if (Platform.isIOS) {
+      requestIOSPermission();
+    }
+
+    initializePlatform();
+  }
+
+  requestIOSPermission() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        .requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+  }
+
+  initializePlatform() {
+    var initSettingAndroid = AndroidInitializationSettings('app_icon');
+    var initSettingIOS = IOSInitializationSettings(
+      requestSoundPermission: true,
+      requestBadgePermission: true,
+      requestAlertPermission: true,
+      onDidReceiveLocalNotification: (id, title, body, payload) async {
+        ReceiveNotification notification = ReceiveNotification(
+            id: id, title: title, body: body, payload: payload);
+        didReceiveLocalNotificationSubject.add(notification);
+      },
+    );
+
+    initSetting = InitializationSettings(initSettingAndroid, initSettingIOS);
+  }
+
+  setOnNotificationReceive(Function onNotificationReceive) {
+    didReceiveLocalNotificationSubject.listen((notification) {
+      onNotificationReceive(notification);
+    });
+  }
+
+  setOnNotificationClick(Function onNotificationClick) async {
+    await flutterLocalNotificationsPlugin.initialize(initSetting,
+        onSelectNotification: (String payload) async {
+      onNotificationClick(payload);
+    });
+  }
+
+  Future<void> showNotification({
+    @required String title,
+    @required String body,
+    @required String payload,
+  }) async {
+    var androidChannel = AndroidNotificationDetails(
+      'CHANNEL_ID',
+      'CHANNEL_NAME',
+      'CHANNEL_DESCRIPTION',
+      importance: Importance.Max,
+      priority: Priority.High,
+      playSound: true,
+    );
+    var iosChannel = IOSNotificationDetails();
+    var platformChannel = NotificationDetails(androidChannel, iosChannel);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannel,
+      payload: payload,
+    );
+  }
+}
+
+class ReceiveNotification {
+  final int id;
+  final String title;
+  final String body;
+  final String payload;
+
+  ReceiveNotification({
+    @required this.id,
+    @required this.title,
+    @required this.body,
+    @required this.payload,
+  });
+}
